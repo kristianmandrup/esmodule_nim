05d63a513ff8d539a215f193b5315e09b522a720
Kristian Mandrup <kmandrup@gmail.com>
2020-01-29 20:55:53 +0000

    use template with emit to fully control output :)

--- src/esmodule.nim
+++ src/esmodule.nim
@@ -12,6 +12,14 @@ proc esImportDefaultAs*(name: cstring, nameOrPath: cstring) {.
     importcpp: "import { default as # } from #".}
 
 # import { x } from 'xyz'
-proc esImport*(name: cstring, nameOrPath: cstring) {.
-    importcpp: "import { # } from #".}
+# proc esImport*(name: cstring, nameOrPath: cstring) {.
+#     importcpp: "import { # } from #".}
 
+proc esImportImpl(name: string, nameOrPath: string): string =
+  result = "import { " & name & " } from "
+  result.addQuoted nameOrPath
+
+template esImport*(name: string, nameOrPath: string) =
+  {.emit: esImportImpl(name, nameOrPath).}
+
+# esImport("foo", "bar")
